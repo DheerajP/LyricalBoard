7cf9fb8cccb2a2b95347a188b9e47824c02c3b62
Dheeraj Palagiri <dheeraj.palagiri@gmail.com>
2022-05-10 17:02:10 +0100

    updates

--- components/LyricList.js
+++ components/LyricList.js
@@ -5,11 +5,13 @@ export default function LyricList({ lyrics }) {
         <div className={lyricStyles.grid}>
 
             {lyrics.map((lyric) => (
-                <>
-                    <h3>{lyric.internalName}</h3>
-                    <p>{lyric.lyric}</p>
-                    <a href={lyric.slug}>More</a>
-                </>
+                <div className={lyricStyles.card}>
+                    <a href={lyric.slug}>
+                        <h3>{lyric.internalName}</h3>
+                        <p>{lyric.lyric}</p>
+                        <br />
+                        Discuss?</a>
+                </div>
             ))}
 
         </div>
